8a496cd1211ff631ab67bac167c4107e13f33fde
blueblank <blueblank@gmail.com>
2012-06-02 09:22:05 -0400

    a problem

--- modules/myapi.py
+++ modules/myapi.py
@@ -8,21 +8,8 @@ class MyAPI(MethodView):
 
     def get(self, resource_tag):
         #return jsonify(here="here")
-        return get_resource.apply_async(resource_tag)
-        #return get_document.apply_async(args = [ database.db_session,
-        #                                         session.get('public_token', None),
-        #                                         request.form.get('return', "raw"),
-        #                                         document_tag ] )      
+        return get_resource.apply_async(resource_tag)  
 
 @task(name="get_resource")
 def get_resource(resource_tag):
-#def get_document(session, token, return_as, doc_tag):
     pass
-    #print dir(celery)
-    #print type(celery)
-    #print celery.conf
-    #try:
-    #    d = Document.get_by_filtered(session, "part_of", token, "tag", doc_tag)
-    #    return jsonify(tag = str(d.tag), document = getattr(d,return_as)() )
-    #except:
-    #    return jsonify(error="That document could not be returned")
